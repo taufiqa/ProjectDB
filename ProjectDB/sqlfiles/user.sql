3dcbc09ecd8c4658a4d0d401829d67e78b001734
taufiqa <41548419+taufiqa@users.noreply.github.com>
2019-12-06 11:14:04 -0500

    Removed Categories table

--- ProjectDB/sqlfiles/user.sql
+++ ProjectDB/sqlfiles/user.sql
@@ -22,7 +22,7 @@ select * from users;
 
 CREATE TABLE items (
 	itemID INTEGER NOT NULL auto_increment,
-    userID INTEGER,
+    userID INTEGER NOT NULL,
     title VARCHAR(20),
     itemDescription VARCHAR(100),
     postDate DATE, 
@@ -31,29 +31,21 @@ CREATE TABLE items (
     PRIMARY KEY(itemID),
     FOREIGN KEY(userID) REFERENCES users(userID)
 );
-set foreign_key_checks = 0;
+set foreign_key_checks = 1;
 drop table items;
 DELETE FROM items;
 select * from items;
+select itemID,title,itemDescription,postDate,price,categoryName from items WHERE categoryName = "electronics";
 
-CREATE TABLE categories (
-	categoryID INTEGER NOT NULL auto_increment,
-    categoryName VARCHAR(15),
-    itemTitle VARCHAR(20),
-    PRIMARY KEY(categoryID, itemTitle)
-);
-drop table categories;
-DELETE FROM categories;
-select * from categories;
-
-SELECT * FROM Items INNER JOIN categories ON Items.title = Categories.itemTitle;
+insert into items values(
+1, 2, "smartphone", "is aiight", "2019-12-05", 100.00, "electronics");
 
 CREATE TABLE reviews (
 	reviewID INTEGER NOT NULL auto_increment,
     score VARCHAR(10),
     remark VARCHAR(75),
-    itemID integer,
-    userID integer,
+    itemID integer not null,
+    userID integer not null,
     reviewDate DATE, 
     PRIMARY KEY (reviewID),
     FOREIGN KEY (userID) REFERENCES users(userID),
