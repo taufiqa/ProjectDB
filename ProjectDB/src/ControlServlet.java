bbb3f2a0a1ca96996e4ccb7dd292d2ca0f4cc390
nuhamal <55333296+nuhamal@users.noreply.github.com>
2019-12-07 02:22:43 -0500

    Kinda Sorta Working Search - still figuring it out

--- ProjectDB/src/ControlServlet.java
+++ ProjectDB/src/ControlServlet.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -7,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List; 
@@ -16,6 +18,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
 
 public class ControlServlet extends HttpServlet
 {
@@ -59,6 +63,9 @@ public class ControlServlet extends HttpServlet
 	            	break;
 	            case "/newItem":
 	            	newItem(request,response);
+	            	break;
+	            case "/search":
+	            	search(request,response);
 	            	break;
 	            default:
 	            	break;
@@ -143,21 +150,24 @@ public class ControlServlet extends HttpServlet
 		LocalDate postDate = LocalDate.now();
 		double price = Double.parseDouble(request.getParameter("price"));
 		String categoryName = request.getParameter("categoryName").toLowerCase();
-		String userEmail = user.getEmail();    
+		String userEmail = user.getEmail();   
 		
+		response.sendRedirect("StorePage.jsp");
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdb?", "root", "Sql786!!");
 			Statement st=connect.createStatement();
 			
-		int i=st.executeUpdate("insert into items(userEmail,title,itemDescription,postDate,price)values('" + userEmail + "','" +title + "','" + itemDescription + "','" + postDate +"','"+ price + "')");
-		int j = st.executeUpdate("insert into categories(categoryName,itemTitle)values('" + categoryName + "','" + title + "')");
+		int i=st.executeUpdate("insert into items(userEmail,title,itemDescription,postDate,price,"
+				+ "categoryName)values('" + userEmail + "','" +title + "','" + itemDescription + "','" + 
+				postDate +"','"+ price  +"','"+ categoryName + "')");
 		
-		if (i != 0)response.sendRedirect("success.jsp");
-		else {
+		//WORK ON REDIRECTION
+		/*if (i != 0)response.sendRedirect("success.jsp");
+		else if (i == 0){
 			response.sendRedirect("error.jsp"); //make an error.jsp
-			}
+			}*/
 		}
 		catch(Exception e)
 		{
@@ -166,14 +176,104 @@ public class ControlServlet extends HttpServlet
 		}
 
 	}
-/*
-	private void searchCategory(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException
+
+	private void search(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException
 	{
-		List<Items> itemsList = new ArrayList<Items>();
 		
 		String category = request.getParameter("searchCategory");
 		
-		itemsList = ItemsDAO.getItemCategoryList(category);
+		//take the category they searched for and look in the items table
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdb?", "root", "Sql786!!");
+		PrintWriter printOut = response.getWriter();
+		
+String searchQuery = "select * from items where categoryName = ?";    
+		
+		preparedStatement = (PreparedStatement)connect.prepareStatement(searchQuery);	
+		preparedStatement.setString(1, category);
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		java.sql.ResultSetMetaData metaData = resultSet.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		
+
+		response.setContentType("text/html");
+		
+		for (int j = 2; j < columnCount + 1; j++)
+		{
+			printOut.print(metaData.getColumnName(j) + "   ");
+		}
+		
+		printOut.println();
+		
+		
+		//trying to parse category
+		String selectQuery = "select categoryName from items";
+		PreparedStatement preparedStatement1 = (PreparedStatement)connect.prepareStatement(selectQuery);
+		ResultSet resultSet1 = preparedStatement1.executeQuery();
+		java.sql.ResultSetMetaData metaData1 = resultSet1.getMetaData();
+		int columnCount1 = metaData1.getColumnCount();
+		
+		String smth = "";
+		while (resultSet1.next())
+		{
+			for (int j = 0; j < columnCount1; j++)
+			{
+				String columnValue1 = resultSet1.getString(j);
+				for (int i = 0; i < columnValue1.length(); i++)
+				{
+					if (columnValue1.charAt(i) != ',')
+					{
+						smth += columnValue1.charAt(i);
+					}
+					if (smth == category)
+					{
+						for (int k = 2; k < columnCount + 1; k++)
+						{
+							String columnValue = resultSet.getString(k);
+							printOut.print(columnValue + "   ");
+						}
+					}
+				}
+			}
+		}
+		
+
+		//current functionality
+		
+
+		/*
+		 * for (int j = 2; j < columnCount + 1; j++)
+		{
+			printOut.print(metaData.getColumnName(j) + "   ");
+		}
+		
+		printOut.println();
+		
+		 * while (resultSet.next())
+		{
+			for (int k = 2; k < columnCount + 1; k++)
+			{
+				String columnValue = resultSet.getString(k);
+				printOut.print(columnValue + "   ");
+			}
+			printOut.println();
+		}*/
+		
+		//response.sendRedirect("StorePage.jsp");
+		
+		}
+		
+		catch(Exception e)
+		{
+		System.out.print(e);
+		e.printStackTrace();
+		}
+		/*itemsList = ItemsDAO.getItemCategoryList(category);
 		
 		if (!category.isEmpty())
 		{
@@ -186,7 +286,8 @@ public class ControlServlet extends HttpServlet
 		{
 			System.out.println("else statement");
 			//allItems(request, response);
-		}
+		}*/
+		
 	}
-	*/
+	
 }
