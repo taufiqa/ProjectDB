fdcab7687c121de26b37716740cbcea4a8a28a67
nuhamal <55333296+nuhamal@users.noreply.github.com>
2019-12-07 21:01:46 -0500

    Add files via upload

--- Project.sql
+++ Project.sql
@@ -1,27 +1,22 @@
-CREATE DATABASE onlineStore;
+CREATE DATABASE projectdb;
 
-USE onlineStore;
+USE projectdb;
 
 CREATE TABLE items (
 	itemID INTEGER NOT NULL auto_increment,
+    userEmail VARCHAR(25),
     title VARCHAR(20),
     itemDescription VARCHAR(100),
     postDate DATE,
     price DECIMAL(5, 2),
+	categoryName VARCHAR(50),
     PRIMARY KEY(itemID)
 );
 
-CREATE TABLE categories (
-	categoryID INTEGER NOT NULL auto_increment,
-    categoryName VARCHAR(15),
-    itemID integer,
-    PRIMARY KEY(categoryID, itemID),
-    FOREIGN KEY (itemID) REFERENCES items(itemID), /*itemID here references itemID in items
-													table, there will be multiple rows in table 
-                                                    categories, one for each item*/
-	CHECK (binary_checksum(categoryName) = binary_checksum(lower(categoryName)))
-    /*check to ensure that category names are lowercase*/
-);
+DROP TABLE items;
+DELETE FROM users;
+SELECT * FROM items;
+
 CREATE TABLE users (
 	userID INTEGER NOT NULL auto_increment,
     firstName VARCHAR(25),
@@ -30,17 +25,20 @@ CREATE TABLE users (
     email VARCHAR(25),
     gender CHAR(2),
     age INTEGER,
-    PRIMARY KEY(userID),
+    PRIMARY KEY(userID, email),
     CHECK (gender IN ('M', 'F'))
 );
+
+DROP TABLE users;
 CREATE TABLE reviews (
 	reviewID INTEGER NOT NULL auto_increment,
     score VARCHAR(10),
     remark VARCHAR(75),
     itemID integer,
-    userID integer,
+    userEmail VARCHAR(25),
     reviewDate DATE,
-    PRIMARY KEY (reviewID),
-    CHECK (((SELECT COUNT (userID) FROM reviews) <= 5) WHILE reviewDate )
+    PRIMARY KEY (reviewID)
 );
+
 SELECT * FROM users;
+DELETE FROM items; DELETE FROM categories;
