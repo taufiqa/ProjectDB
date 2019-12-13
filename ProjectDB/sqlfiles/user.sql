e0fba158791450805daba35337d366cc222cfe3a
taufiqa <41548419+taufiqa@users.noreply.github.com>
2019-12-12 22:13:20 -0500

    new files

--- ProjectDB/sqlfiles/user.sql
+++ ProjectDB/sqlfiles/user.sql
@@ -1,5 +1,5 @@
 use projectdb;
-
+create database projectdb;
 CREATE TABLE users (
 	userID INTEGER NOT NULL auto_increment,
     firstName VARCHAR(25),
@@ -8,7 +8,7 @@ CREATE TABLE users (
     email VARCHAR(25),
     gender CHAR(2),
     age INTEGER,
-    PRIMARY KEY(userID),
+    PRIMARY KEY(userID,email),
     CHECK (gender IN ('M', 'F'))
 );
 
@@ -22,23 +22,22 @@ select * from users;
 
 CREATE TABLE items (
 	itemID INTEGER NOT NULL auto_increment,
-    userID INTEGER NOT NULL,
+    userEmail VARCHAR(25),
     title VARCHAR(20),
     itemDescription VARCHAR(100),
     postDate DATE, 
     price DECIMAL(9, 2),
     categoryName VARCHAR(30),
-    PRIMARY KEY(itemID),
-    FOREIGN KEY(userID) REFERENCES users(userID)
+    PRIMARY KEY(itemID)
 );
 set foreign_key_checks = 1;
 drop table items;
-DELETE FROM items;
+DELETE userID FROM items;
 select * from items;
 select itemID,title,itemDescription,postDate,price,categoryName from items WHERE categoryName = "electronics";
 
 insert into items values(
-1, 2, "smartphone", "is aiight", "2019-12-05", 100.00, "electronics");
+4, "nuhamal@gmail.com", "talking book", "it's a book that talks", "2019-12-08", 500.00, "electronics,books");
 
 CREATE TABLE reviews (
 	reviewID INTEGER NOT NULL auto_increment,
@@ -54,3 +53,27 @@ CREATE TABLE reviews (
 drop table reviews;
 DELETE FROM reviews;
 select * from reviews;
+
+CREATE TABLE favUsers (
+	userID int NOT NULL, 
+    favUserID int NOT NULL,
+    userEmail varchar(25),
+    favUserEmail varchar(25),
+    PRIMARY KEY(userID,favUserID), 
+    FOREIGN KEY(userID) REFERENCES users(userID),
+    FOREIGN KEY(favUserID) REFERENCES users(userID)
+);
+insert into favUsers values(2, 3);
+select * from users;
+select * from favUsers;
+
+CREATE TABLE favItems (
+	itemID int NOT NULL, 
+    userID int NOT NULL, 
+    PRIMARY KEY(itemID,userID), 
+    FOREIGN KEY(itemID) REFERENCES Items(itemID), 
+    FOREIGN KEY(userID) REFERENCES Users(userID)
+);
+drop table favItems;
+insert into favItems values(2, 2, "smartphone");
+select * from favItems;
